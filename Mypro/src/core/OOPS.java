6d048a0634a92ce023c4a0d555a560a67c22ca84
Training <Training@DS-9111AA013483>
2019-04-25 12:20:48 +0530

    class created OOps.java

---
+++ Mypro/src/core/OOPS.java
@@ -0,0 +1,10 @@
+package core;
+
+public class OOPS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("successfully join with github");
+	}
+
+}
